2c4cb8d4e7501207d6d16bfc1d3153c763962785
Jerett Latimer <jerettlatimer@yahoo.com>
2019-11-07 09:49:19 -0800

    test

---
+++ src/game/Player.java
@@ -0,0 +1,7 @@
+package game;
+
+public class Player {
+	public Player() {
+		
+	}
+}
---
+++ src/game/Room.java
@@ -0,0 +1,44 @@
+package game;
+
+public class Room {
+private Door north, east, south, west;
+	
+	public Room(Door northDoor, Door eastDoor, Door southDoor, Door westDoor) {
+		this.setNorth(northDoor);
+		this.setEast(eastDoor);
+		this.setSouth(southDoor);
+		this.setWest(westDoor);
+	}
+
+	public Door getNorth() {
+		return north;
+	}
+
+	public void setNorth(Door north) {
+		this.north = north;
+	}
+
+	public Door getEast() {
+		return east;
+	}
+
+	public void setEast(Door east) {
+		this.east = east;
+	}
+
+	public Door getSouth() {
+		return south;
+	}
+
+	public void setSouth(Door south) {
+		this.south = south;
+	}
+
+	public Door getWest() {
+		return west;
+	}
+
+	public void setWest(Door west) {
+		this.west = west;
+	}
+}
